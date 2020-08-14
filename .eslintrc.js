491eb6a4fda19afa3b0d9ed89e9e6216b32ad7bc
VitorRedfox <vitor_pereira.2013@hotmail.com>
2020-08-14 00:17:44 -0300

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'func-names': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.d.ts'],
+      },
+    },
+  },
+};
--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,14 @@
 export class Game {
   private context: CanvasRenderingContext2D;
+
   private width: number;
+
   private height: number;
 
   constructor(
     width: number,
     height: number,
-    context: CanvasRenderingContext2D
+    context: CanvasRenderingContext2D,
   ) {
     this.width = width;
     this.height = height;
@@ -18,21 +20,21 @@ export class Game {
   }
 
   private renderBoard() {
-    this.context.fillStyle = "black";
+    this.context.fillStyle = 'black';
     this.context.fillRect(0, 0, this.width, this.height);
 
     const columns = 10;
     const w = this.width / columns;
     const rows = this.height / w;
 
-    for (var i = 0; i < 10; i++) {
-      for (var j = 0; j < rows; j++) {
-        this.context.fillStyle = "#2e2e2e";
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < rows; j++) {
+        this.context.fillStyle = '#2e2e2e';
         this.context.fillRect(w * i, w * j, w - 0.2, w - 0.2);
       }
     }
 
-    this.context.fillStyle = "#e3213b";
+    this.context.fillStyle = '#e3213b';
     this.context.fillRect(w * 3, w * 3, w - 0.2, w - 0.2);
     this.context.fillRect(w * 3, w * 4, w - 0.2, w - 0.2);
     this.context.fillRect(w * 4, w * 4, w - 0.2, w - 0.2);
@@ -44,12 +46,10 @@ export class Game {
     y: number,
     width: number,
     height: number,
-    radius: number
+    radius: number,
   ) {
-    if (!this.context) throw new Error("Error");
-
-    if (width < 2 * radius) radius = width / 2;
-    if (height < 2 * radius) radius = height / 2;
+    // if (width < 2 * radius) radius = width / 2;
+    // if (height < 2 * radius) radius = height / 2;
     this.context.beginPath();
     this.context.moveTo(x + radius, y);
     this.context.arcTo(x + width, y, x + width, y + height, radius);
